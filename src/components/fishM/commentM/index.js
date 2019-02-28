037dd9813cf7c4c908c3c682030b5a4e3906f0ba
wjq990112 <1163585385@qq.com>
2019-02-28 21:22:32 +0800

    更新鱼群管理和系统管理

--- src/components/fishM/commentM/index.js
+++ src/components/fishM/commentM/index.js
@@ -1,46 +1,35 @@
 import React from 'react';
-import { Card, Input, DatePicker, Button, Row, Col, Modal, Select,Table } from 'antd';
+import { Card, Input, DatePicker, Button, Row, Col, Modal, Table } from 'antd';
 import BreadcrumbCustom from '../../BreadcrumbCustom';
 import 'antd/dist/antd.css';
 
 const { RangePicker } = DatePicker;
 const confirm = Modal.confirm;
-const Option = Select.Option;
 class CommentM extends React.Component {
-  state = {
-    selectedRowKeys: [], // Check here to configure the default column
-    commentData: [{
-      // key: 1,
-      number: 1,
-      bookname: '钢铁是怎样炼成的',
-      comment: '吉利李书福占戴勒姆近10%股份',
-      commentpeople: '胡晓雪',
-      time: '2018-02-26 15:25:00'
-    },
-    {
-      // key: 2,
-      number: 2,
-      bookname: '我的好妈妈',
-      comment: '两会代表就房产税提议：2019年北京开始试点',
-      commentpeople: '胡晓雪',
-      time: '2018-02-26 13:54:00'
-    },
-    {
-      // key: 3,
-      number: 3,
-      bookname: 'dasda',
-      comment: 'asdasd',
-      commentpeople: 'adsad',
-      time: 'asdasd'
-    }
-  ]
-  };
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedRowKeys: [], // Check here to configure the default column
+      commentData: [{
+        number: 1,
+        bookname: '钢铁是怎样炼成的',
+        comment: '吉利李书福占戴勒姆近10%股份',
+        commentpeople: '胡晓雪',
+        time: '2018-02-26 15:25:00'
+      },
+      {
+        number: 2,
+        bookname: '我的好妈妈',
+        comment: '两会代表就房产税提议：2019年北京开始试点',
+        commentpeople: '胡晓雪',
+        time: '2018-02-26 13:54:00'
+      }],
+    };
     this.dateRangeChange = this.dateRangeChange.bind(this);
     this.findBtnClick = this.findBtnClick.bind(this);
-
-    this.handleChange = this.handleChange.bind(this);
+    this.deleteCommentBtnClick = this.deleteCommentBtnClick.bind(this);
   }
   onSelectChange = (selectedRowKeys) => {
     console.log('selectedRowKeys changed: ', selectedRowKeys);
@@ -54,40 +43,35 @@ class CommentM extends React.Component {
     console.log('Find');
   }
 
-  deleteCommentBtnClick = ()=>{
+  deleteCommentBtnClick() {
     confirm({
       okText: '删除',
       cancelText: '取消',
-      content: `是否确定删除这${this.state.selectedRowKeys}条评论？`,
-      onOk: ()=>{
+      content: `是否确定删除这 ${this.state.selectedRowKeys.length} 条评论`,
+      onOk: () => {
         let tmp = this.state.selectedRowKeys.sort();
-        console.log('删除前tmp',tmp);
-        // console.log('onok之前的',tmp)
         let len = tmp.length;
         let cnt = 0;
         let data = this.state.commentData;
-        for(let i = 0; i < len; i++) {
-          tmp[i]-=cnt;
-          data.splice(tmp[i],1);
+        for (let i = 0; i < len; i++) {
+          tmp[i] -= cnt;
+          data.splice(tmp[i], 1);
           cnt++;
         }
-        this.setState({selectedRowKeys:[],commentData:data})
+        this.setState({ selectedRowKeys: [], commentData: data })
+
         console.log(data);
       },
-      onCancel: ()=>{
+      onCancel: () => {
         console.log('Cancel');
       },
     });
   }
 
-  handleChange(value) {
-    console.log(value);
-  }
-
   render() {
     const { selectedRowKeys } = this.state;
     const rowSelection = {
-      selectedRowKeys,
+      selectedRowKeys: selectedRowKeys.sort(),
       onChange: this.onSelectChange,
       // onSelection: this.onSelection,
     };
@@ -108,7 +92,7 @@ class CommentM extends React.Component {
       title: '评论时间',
       dataIndex: 'time'
     }];
-    
+
 
     return (
       <React.Fragment>
@@ -118,7 +102,7 @@ class CommentM extends React.Component {
             <div>
               <Input
                 placeholder="标题/内容模糊查询"
-                style={{ width: '180px', marginLeft: '24px' }}
+                style={{ width: '180px' }}
               />
               <Input
                 placeholder="评论人"
@@ -140,33 +124,24 @@ class CommentM extends React.Component {
               <Button
                 type="primary"
                 onClick={this.deleteCommentBtnClick}
-                style={{ marginLeft: '24px', marginTop: '24px', marginRight: '10px' }}
+                style={{ marginTop: '24px', marginRight: '10px' }}
                 disabled={!hasSelected}
               >
                 删除评论
               </Button>
-              第 1 条到第 2 条，共 2 条  每页显示
-              <Select
-                defaultValue="10"
-                onChange={this.handleChange}
-                style={{ width: '65px', marginLeft: '10px', marginRight: '10px' }}
-              >
-                <Option value="10">10</Option>
-                <Option value="20">20</Option>
-                <Option value="50">50</Option>
-              </Select>
-              条
             </div>
-            <div>
-              <Row gutter={16}>
-                <Col className="gutter-row" md={24}>
-                  <div className="gutter-box">
-                    <Card bordered={false}>
-                      <Table rowSelection={rowSelection} columns={columns} dataSource={this.state.commentData} />
-                    </Card>
-                  </div>
-                </Col>
-              </Row>
+            <div className="gutter-box" style={{ marginTop: '10px' }}>
+              <Table
+                bordered
+                rowSelection={rowSelection}
+                columns={columns}
+                dataSource={this.state.commentData}
+                pagination={{
+                  showTotal: (total, range) => `第 ${range[0]} 条到第 ${range[1]} 条，共 ${total} 条`,
+                  showSizeChanger: true,
+                  pageSizeOptions: ['10', '20', '50']
+                }}
+              />
             </div>
           </Card>
         </div>
