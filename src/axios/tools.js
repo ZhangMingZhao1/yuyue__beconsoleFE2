0baaceb9ae8b42a7741e34cf56ad2a569a99b365
funics <p19991112@qq.com>
2019-03-01 16:52:54 +0800

    fetch封装

--- src/axios/tools.js
+++ src/axios/tools.js
@@ -3,7 +3,7 @@
  * http通用工具函数
  */
 import axios from 'axios';
-import { message } from 'antd';
+import { message, Modal } from 'antd';
 
 /**
  * 公用get请求
@@ -11,10 +11,10 @@ import { message } from 'antd';
  * @param msg       接口异常提示
  * @param headers   接口所需header配置
  */
-export const get = ({url, msg = '接口异常', headers}) =>
+export const get = ({ url, msg = '接口异常', headers }) =>
     axios.get(url, headers).then(res => res.data).catch(err => {
-       console.log(err);
-       message.warn(msg);
+        console.log(err);
+        message.warn(msg);
     });
 
 /**
@@ -24,8 +24,60 @@ export const get = ({url, msg = '接口异常', headers}) =>
  * @param msg       接口异常提示
  * @param headers   接口所需header配置
  */
-export const post = ({url, data, msg = '接口异常', headers}) =>
+export const post = ({ url, data, msg = '接口异常', headers }) =>
     axios.post(url, data, headers).then(res => res.data).catch(err => {
         console.log(err);
         message.warn(msg);
     });
+
+export const fetchGet = (options) => {
+    let base = 'https://easy-mock.com/mock/5c78d39c141be85de9c0add9';
+    let params = options.data && options.data.param ? '?' + parseParams(options.data.params) : '';
+    let url = base + options.url + params;
+    console.log(url);
+
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json',
+                'mode': 'no-cors'
+            },
+            credentials: 'include', // 请求带上cookies，是每次请求保持会话一直
+        }).then(
+            (res) => {
+                if (res.status == '200') {//http请求成功
+                    return res.json()
+                } else {
+                    reject(res);
+                }
+            }
+        ).then(data => {
+            if (data.code == '0') {//业务成功标识
+                resolve(data);
+            } else {
+                Modal.info({
+                    title: "提示",
+                    content: data.msg
+                })
+            }
+        }).catch((err) => {
+            console.log('fetch error' + err);
+        })
+    });
+}
+
+function parseParams(data) {
+    try {
+        let tempArr = [];
+        for (let i in data) {
+            let key = encodeURIComponent(i);
+            let value = encodeURIComponent(data[i]);
+            tempArr.push(key + '=' + value);
+        }
+        let urlParamsStr = tempArr.join('&');
+        return urlParamsStr;
+    } catch (err) {
+        return '';
+    }
+}
