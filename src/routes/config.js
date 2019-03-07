19eeb81acf0d17b7b23e1d0a431d4c534f511bf6
ZhangMingZhao1 <1104272319@qq.com>
2019-03-07 19:17:43 +0800

    删除一些

--- src/routes/config.js
+++ src/routes/config.js
@@ -91,38 +91,6 @@ export default {
         { key: '/app/systemM/staffM', title: '员工管理', component: 'StaffM' }
       ],
     },
-    {
-      key: '/app/table', title: '表格', icon: 'copy',
-      subs: [
-        { key: '/app/table/basicTable', title: '基础表格', component: 'BasicTable' },
-        { key: '/app/table/advancedTable', title: '高级表格', component: 'AdvancedTable' },
-        { key: '/app/table/asynchronousTable', title: '异步表格', component: 'AsynchronousTable' },
-      ],
-    },
-    {
-      key: '/app/chart', title: '图表', icon: 'area-chart',
-      subs: [
-        { key: '/app/chart/echarts', title: 'echarts', component: 'Echarts' },
-        { key: '/app/chart/recharts', title: 'recharts', component: 'Recharts' },
-      ],
-    },
-    {
-      key: '/subs4', title: '页面', icon: 'switcher',
-      subs: [
-        { key: '/login', title: '登录' },
-        { key: '/404', title: '404' },
-      ],
-    },
-    {
-      key: '/app/auth', title: '权限管理', icon: 'safety',
-      subs: [
-        { key: '/app/auth/basic', title: '基础演示', component: 'AuthBasic' },
-        { key: '/app/auth/routerEnter', title: '路由拦截', component: 'RouterEnter', auth: 'auth/testPage' },
-      ],
-    },
-    {
-      key: '/app/cssModule', title: 'cssModule', icon: 'star', component: 'Cssmodule'
-    },
   ],
   others: [
     {
