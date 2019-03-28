b73c139ddf01476ad0eb14a2991067f2a42835e7
wjq990112 <1163585385@qq.com>
2019-03-28 22:19:04 +0800

    评论管理界面重绘（暂未与后端联调）

--- src/components/fishM/commentM/index.js
+++ src/components/fishM/commentM/index.js
@@ -1,166 +1,227 @@
 import React from 'react';
-import { Card, Input, DatePicker, Button, Modal, Table, message } from 'antd';
+import { Card, Input, DatePicker, Button, Modal, Table, message, Tabs } from 'antd';
 import BreadcrumbCustom from '../../BreadcrumbCustom';
 import 'antd/dist/antd.css';
 
 const { RangePicker } = DatePicker;
 const confirm = Modal.confirm;
+const TabPane = Tabs.TabPane;
+
 class CommentM extends React.Component {
 
-  constructor(props) {
-    super(props);
+	state = {
+		dynamic: [],
+		commnet: [],
+		selectedRowKeys1: [],  // Check here to configure the default column
+		selectedRowKeys2: [],  // Check here to configure the default column
+	}
 
-    this.state = {
-      selectedRowKeys: [], // Check here to configure the default column
-      commentData: []
-    };
-    this.dateRangeChange = this.dateRangeChange.bind(this);
-    this.findBtnClick = this.findBtnClick.bind(this);
-    this.deleteCommentBtnClick = this.deleteCommentBtnClick.bind(this);
-  }
+	componentDidMount() {
+		this.requestList();
+	}
 
+	onSelect1Change = (selectedRowKeys) => {
+		console.log('selectedRowKeys1 changed: ', selectedRowKeys);
+		this.setState({ selectedRowKeys1: selectedRowKeys });
+	}
 
-  componentDidMount() {
-    this.requestList();
-  }
+	onSelect2Change = (selectedRowKeys) => {
+		console.log('selectedRowKeys2 changed: ', selectedRowKeys);
+		this.setState({ selectedRowKeys2: selectedRowKeys });
+	}
 
-  onSelectChange = (selectedRowKeys) => {
-    console.log('selectedRowKeys changed: ', selectedRowKeys);
-    this.setState({ selectedRowKeys });
-  };
+	dynamicDelete = () => {
 
-  dateRangeChange() {
-    console.log('DateRangeChanged');
-  }
+	}
 
-  findBtnClick() {
-    console.log('Find');
-  }
+	requestList = () => {
+		const url = 'xxx/yuyue/listUserdynamic';
+		fetch(url)
+			.then((res) => res.json())
+			.then(data => {
+				// eslint-disable-next-line
+				console.log(data);
+				// data.data.data.map((item, index) => {
+				// 	item.key = index;
+				// });
+			})
+			.catch(err => {
+				console.log('fetch error', err)
+			});
+	}
 
-  deleteCommentBtnClick() {
-    confirm({
-      okText: '删除',
-      cancelText: '取消',
-      content: `是否确定删除这 ${this.state.selectedRowKeys.length} 条评论`,
-      onOk: () => {
-        let tmp = this.state.selectedRowKeys.sort();
-        let len = tmp.length;
-        let cnt = 0;
-        let data = this.state.commentData;
-        for (let i = 0; i < len; i++) {
-          tmp[i] -= cnt;
-          data.splice(tmp[i], 1);
-          cnt++;
-        }
-        this.setState({ selectedRowKeys: [], commentData: data })
-        console.log(data);
-      },
-      onCancel: () => {
-        console.log('Cancel');
-      },
-    });
-  }
+	render() {
 
-  requestList = () => {
-    const url = 'http://119.3.231.11:8080/yuyue/listUserdynamic';
-    fetch(url)
-      .then((res) => res.json())
-      .then(data => {
-        console.log(data.content);
-        // eslint-disable-next-line
-        data.content.map((item, index) => {
-          item.key = index;
-        });
-        this.setState({
-          commentData: data.content
-        });
-      })
-      .catch(err => {
-        console.log('fetch error', err)
-      });
-  }
+		const { selectedRowKeys1, selectedRowKeys2 } = this.state;
 
-  render() {
-    const { selectedRowKeys } = this.state;
-    const rowSelection = {
-      selectedRowKeys: selectedRowKeys.sort(),
-      onChange: this.onSelectChange,
-      // onSelection: this.onSelection,
-    };
-    const hasSelected = selectedRowKeys.length > 0;
-    const columns = [{
-      title: '序号',
-      dataIndex: 'dynamicId',
-    }, {
-      title: '书名',
-      dataIndex: 'bsBookinfo.bookName',
-    }, {
-      title: '评论内容',
-      dataIndex: 'content',
-    }, {
-      title: '评论人',
-      dataIndex: 'commentpeople'
-    }, {
-      title: '评论时间',
-      dataIndex: 'bsBookinfo.createTime'
-    }];
+		const rowSelection1 = {
+			selectedRowKeys1: selectedRowKeys1.sort(),
+			onChange: this.onSelect1Change,
+			// onSelection: this.onSelection,
+		};
 
+		const rowSelection2 = {
+			selectedRowKeys2: selectedRowKeys2.sort(),
+			onChange: this.onSelect2Change,
+			// onSelection: this.onSelection,
+		};
 
-    return (
-      <React.Fragment>
-        <BreadcrumbCustom first="鱼群管理" second="评论管理" />
-        <div>
-          <Card title="评论管理">
-            <div>
-              <Input
-                placeholder="标题/内容模糊查询"
-                style={{ width: '180px' }}
-              />
-              <Input
-                placeholder="评论人"
-                style={{ width: '130px', marginLeft: '10px' }}
-              />
-              <RangePicker
-                style={{ width: '250px', marginLeft: '10px' }}
-                onChange={this.dateRangeChange}
-              />
-              <Button
-                type="primary"
-                style={{ marginLeft: '10px' }}
-                onClick={this.findBtnClick}
-              >
-                查询
-              </Button>
-            </div>
-            <div>
-              <Button
-                type="primary"
-                onClick={this.deleteCommentBtnClick}
-                style={{ marginTop: '24px', marginRight: '10px' }}
-                disabled={!hasSelected}
-              >
-                删除评论
-              </Button>
-            </div>
-            <div className="gutter-box" style={{ marginTop: '10px' }}>
-              <Table
-                bordered
-                rowSelection={rowSelection}
-                columns={columns}
-                dataSource={this.state.commentData}
-                pagination={{
-                  showTotal: (total, range) => `第 ${range[0]} 条到第 ${range[1]} 条，共 ${total} 条`,
-                  showSizeChanger: true,
-                  pageSizeOptions: ['10', '20', '50']
-                }}
-              />
-            </div>
-          </Card>
-        </div>
-      </React.Fragment>
-    );
-  }
+		const columns1 = [{
+			title: '序号',
+			dataIndex: 'num'
+		}, {
+			title: '书名',
+			dataIndex: 'bookName'
+		}, {
+			title: '动态内容',
+			dataIndex: 'contest',
+			width: '320px'
+		}, {
+			title: '发布人',
+			dataIndex: 'people'
+		}, {
+			title: '发布时间',
+			dataIndex: 'time'
+		}, {
+			title: '操作',
+			dataIndex: 'action',
+			render: (text, record) => (
+				<span>
+					<a href="javascript:;">编辑评论</a>
+				</span>
+			)
+		}];
+
+		const columns2 = [{
+			title: '序号',
+			dataIndex: 'number'
+		}, {
+			title: '评论人',
+			dataIndex: 'commentPeople'
+		}, {
+			title: '评论内容',
+			dataIndex: 'commnet',
+			width: '600px'
+		}, {
+			title: '发布时间',
+			dataIndex: 'commentTime'
+		}];
+
+		const dynamic = [{
+			num: 1,
+			bookName: '《钢铁是怎样炼成的》',
+			contest: '吉利李书福占戴勒姆近10%股份',
+			people: '胡晓雪',
+			time: '2018-02-26 15：25：00'
+		}, {
+			num: 2,
+			bookName: '《钢铁是怎样炼成的》',
+			contest: '两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议了两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议了',
+			people: '胡晓雪',
+			time: '2018-02-26 15：25：00'
+		}];
+
+		const comment = [{
+			number: 1,
+			commentPeople: '啦啦啦',
+			commnet: '两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议了两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议了',
+			commentTime: '2018-02-26 15：25：00'
+		}, {
+			number: 2,
+			commentPeople: '啦啦啦',
+			commnet: '两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议了两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议：2019年北京开始试点两会代表就房产税提议了',
+			commentTime: '2018-02-26 15：25：00'
+		}];
+
+		return (
+			<React.Fragment>
+				<BreadcrumbCustom first="鱼群管理" second="评论管理" />
+				<Card title="评论管理">
+					<Tabs defaultActiveKey="1" onChange={this.callback}>
+						<TabPane tab="动态管理" key="1">
+							<Input
+								placeholder="书名模糊查询"
+								style={{ width: '200px' }}
+							/>
+							<Input
+								placeholder="内容模糊查询"
+								style={{ width: '200px', marginLeft: '10px' }}
+							/>
+							<Input
+								placeholder="发布人"
+								style={{ width: '150px', marginLeft: '10px' }}
+							/>
+							<RangePicker
+								style={{ marginLeft: '10px' }}
+							/>
+							<Button
+								type="primary"
+								style={{ marginLeft: '10px' }}
+							>
+								查询
+							</Button><br />
+							<Button
+								type="primary"
+								style={{ marginTop: '10px' }}
+								onClick={this.dynamicDelete}
+							>
+								批量删除
+							</Button>
+							<Table
+								bordered
+								columns={columns1}
+								dataSource={dynamic}
+								rowSelection={rowSelection1}
+								style={{ marginTop: '10px' }}
+								pagination={{
+									showTotal: (total, range) => `第 ${range[0]} 条到第 ${range[1]} 条，共 ${total} 条`,
+									showSizeChanger: true,
+									pageSizeOptions: ['10', '20', '50']
+								}}
+							/>
+						</TabPane>
+						<TabPane tab="评论管理" key="2">
+							<Input
+								placeholder="内容模糊查询"
+								style={{ width: '200px' }}
+							/>
+							<Input
+								placeholder="发布人"
+								style={{ width: '150px', marginLeft: '10px' }}
+							/>
+							<RangePicker
+								style={{ marginLeft: '10px' }}
+							/>
+							<Button
+								type="primary"
+								style={{ marginLeft: '10px' }}
+							>
+								查询
+							</Button><br />
+							<Button
+								type="primary"
+								style={{ marginTop: '10px' }}
+							>
+								批量删除
+							</Button>
+							<Table
+								bordered
+								columns={columns2}
+								dataSource={comment}
+								rowSelection={rowSelection2}
+								style={{ marginTop: '10px' }}
+								pagination={{
+									showTotal: (total, range) => `第 ${range[0]} 条到第 ${range[1]} 条，共 ${total} 条`,
+									showSizeChanger: true,
+									pageSizeOptions: ['10', '20', '50']
+								}}
+							/>
+						</TabPane>
+					</Tabs>
+				</Card>
+			</React.Fragment>
+		);
+	}
 }
-
 export default CommentM;
 
