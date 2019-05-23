7e49e26b5abd1d2b0e31862a0764f81f2734f55f
funics <p19991112@qq.com>
2019-05-23 10:20:22 +0800

    订单管理/捐书订单接口对接

--- src/components/orderM/donateO/index.js
+++ src/components/orderM/donateO/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Form, Select, Input, Button, Card, DatePicker, Table } from 'antd';
+import { Form, Select, Input, Button, Card, DatePicker, Table, message } from 'antd';
 import BreadcrumbCustom from '../../BreadcrumbCustom';
 import './index.less';
 import OrderDet from './orderDet';
 import Url from '../../../api/config';
 import pagination from '../../pagination';
 import moment from 'moment';
+import { getOptionList, getFormItem } from '../../baseFormItem';
+import { parseParams } from '../../../axios/tools';
 
-const { Option } = Select;
-const InputGroup = Input.Group;
-const { RangePicker } = DatePicker;
 
 //捐书方式
 const deliverTypeConfig = {
@@ -18,6 +17,7 @@ const deliverTypeConfig = {
 }
 //订单状态
 const statusConfig = {
+    "": "全部",
     "-1": "待入柜",
     "0": "待审核",
     "1": "污损",
@@ -25,49 +25,51 @@ const statusConfig = {
     "9": "已完成"
 }
 
+//条件查询Form
 const DonateOSearchForm = Form.create()(
-    (props) => {
-        const { getFieldDecorator } = props.form;
-        const stateSelect = ['全部', '1', '2'];
-        const timeSelect = ['捐书时间', '创建时间'];
-        return (
-            <Form layout="inline">
-                <Form.Item label='状态'>
-                    {getFieldDecorator('state', { initialValue: stateSelect[0] })(
-                        <Select style={{ width: 120 }}>
-                            {stateSelect.map(i => (
-                                <Option key={i} value={i}>{i}</Option>
-                            ))}
-                        </Select>
-                    )}
-                </Form.Item>
-                <Form.Item label="">
-                    {getFieldDecorator('time')(
-                        <InputGroup compact>
-                            <Select defaultValue={timeSelect[0]} style={{ width: 120 }}>
-                                {timeSelect.map(i => (
-                                    <Option key={i} value={i}>{i}</Option>
-                                ))}
-                            </Select>
-                            <RangePicker />
-                        </InputGroup>
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    {getFieldDecorator('fuzzyQuery1')(
-                        <Input placeholder="昵称/账号模糊查询" />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    {getFieldDecorator('fuzzyQuery2')(
-                        <Input placeholder="书籍名称/ISBN模糊查询" />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">查询</Button>
-                </Form.Item>
-            </Form>
-        );
+    class extends React.Component {
+        handleSubmit = (e) => {
+            e.preventDefault();
+            let form = this.props.form;
+            form.validateFields((err, values) => {
+                if (!err) {
+                    console.log(values)
+                    let time = values.time;
+                    let starttime = time && time.timeRange && time.timeRange[0] && time.timeRange[0].valueOf();
+                    let endtime = time && time.timeRange && time.timeRange[0] && time.timeRange[1].valueOf();
+                    let data = {
+                        ...values,
+                        starttime: starttime || '',
+                        endtime: endtime || '',
+                        keyword1: values.keyword1 || '',
+                        keyword2: values.keyword2 || '',
+                    };
+                    delete data.time;
+                    this.props.onSubmit(data);
+                }
+            })
+        }
+        render() {
+            const { form } = this.props;
+            const statusList = [];
+            for (let val in statusConfig) {
+                statusList.push({ id: val, name: statusConfig[val] })
+            }
+            const formList = [
+                { type: 'SELECT', label: '状态', name: 'status', width: '100px', list: statusList },
+                { type: 'OTHER', label: '', name: 'time', component: <TimeGroupInput /> },
+                { type: 'INPUT', label: '', name: 'keyword1', placeholder: "昵称/账号模糊查询" },
+                { type: 'INPUT', label: '', name: 'keyword2', placeholder: "书籍名称/ISBN模糊查询" },
+            ];
+            return (
+                <Form layout="inline" onSubmit={this.handleSubmit}>
+                    {getFormItem(form, formList)}
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">查询</Button>
+                    </Form.Item>
+                </Form>
+            );
+        }
     }
 );
 
@@ -75,19 +77,32 @@ class DonateO extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal1: false,
+            modal: false,//详情弹框
+            donateWay: {},//详情弹框 捐书方式
+            detailData: {},//详情弹框 订单数据
         }
     }
+
     params = {
         currentPage: 1,//当前页面
         pageSize: 10,//每页大小
+        /**搜索参数 */
+        search: {},
     }
+
     componentDidMount() {
         this.requestList();
     }
+
     requestList = () => {
-        fetch(`${Url}/curdonaterecords?start=${this.params.currentPage - 1}&size=${this.params.pageSize}`, { credentials: 'include' })
+        let params = {
+            start: this.params.currentPage - 1,
+            size: this.params.pageSize,
+            ...this.params.search,
+        };
+        fetch(`${Url}/order/curdonaterecords?${parseParams(params)}`, { credentials: 'include' })
             .then((res) => res.json()).then(result => {
+                // if (result.code === 0) {
                 let data = result;
                 this.setState({
                     pagination: pagination(data, (current) => {//改变页码
@@ -98,33 +113,58 @@ class DonateO extends React.Component {
                         this.requestList();
                     }),
                     dataSource: data.content.map(i => ({
+                        ...i,
                         key: i.donateId,
-                        donateId: i.donateId,
-                        userId: i.bsUserinfo.userId,
-                        nickname: i.bsUserinfo.nickname,
-                        deliverType: i.deliverType,
                         donateTime: moment(i.donateTime).format("YYYY-MM-DD HH:mm:ss"),
-                        createTime: moment(i.createTime).format("YYYY-MM-DD HH:mm:ss"),
-                        status: i.status,
+                        // createTime: moment(i.createTime).format("YYYY-MM-DD HH:mm:ss"),
                     }))
+                })
+                // } else {
+                //     message.error(result.message)
+                // }
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
+    /**
+     * 条件查询
+     */
+    handleSearch = (data) => {
+        console.log(data)
+        this.params.search = data;
+        this.requestList();
+    }
+
+    /**
+     * 订单详情
+     */
+    handleDet = (key) => {
+        fetch(`${Url}/order/donaterecords/${key}`, { credentials: 'include' })
+            .then((res) => res.json()).then(data => {
+                this.setState({
+                    detailData: {
+                        ...data,
+                        deliverType: deliverTypeConfig[data.deliverType],
+                        status: statusConfig[data.status],
+                        donateTime: moment(data.donateTime).format("YYYY-MM-DD HH:mm:ss"),
+                        // createTime: moment(i.createTime).format("YYYY-MM-DD HH:mm:ss"),
+                    },
+                    donateWay: data.deliverType === "1" ? "case" : "post",
+                    modal: true
                 })
             }).catch((err) => {
                 console.log(err);
             })
     }
-    showModal(key) {
-        this.setState({ [key]: true });
-    }
-    closeModal(key) {
-        this.setState({ [key]: false });
-    }
 
     render() {
         const columns = [
-            { title: '订单编号', dataIndex: 'donateId' },
-            { title: '会员账号', dataIndex: 'userId' },
-            { title: '会员昵称', dataIndex: 'nickname' },
+            { title: '订单编号', dataIndex: 'orderNo' },
+            { title: '会员账号', dataIndex: 'vipNo' },
+            { title: '会员昵称', dataIndex: 'nikeName' },
             { title: '书籍名称', dataIndex: 'bookName' },
+            { title: 'ISBN', dataIndex: 'isbn' },
             { title: '捐书方式', dataIndex: 'deliverType', render: (type) => deliverTypeConfig[type] },
             { title: '捐书时间', dataIndex: 'donateTime' },
             { title: '创建时间', dataIndex: 'createTime' },
@@ -132,7 +172,7 @@ class DonateO extends React.Component {
             {
                 title: '操作',
                 dataIndex: 'action',
-                render: (text, record) => <span><a onClick={() => { this.showModal('modal1') }}>详情</a></span>
+                render: (text, record) => <span><a onClick={() => { this.handleDet(record.orderNo) }}>详情</a></span>
             }];
 
         return (
@@ -141,7 +181,7 @@ class DonateO extends React.Component {
                 <Card
                     title="捐书订单"
                 >
-                    <DonateOSearchForm />
+                    <DonateOSearchForm onSubmit={this.handleSearch} />
                     <div style={{ textAlign: 'right' }}><Button type='primary'>导出</Button></div><br />
                     <Table
                         className="donateO-table"
@@ -151,9 +191,10 @@ class DonateO extends React.Component {
                         bordered
                     />
                     <OrderDet
-                        visible={this.state.modal1}
-                        onCancel={() => { this.closeModal('modal1') }}
-                        donateWay='post'
+                        visible={this.state.modal}
+                        onCancel={() => { this.setState({ modal: false }) }}
+                        donateWay={this.state.donateWay}
+                        data={this.state.detailData}
                     />
                 </Card>
 
@@ -163,3 +204,64 @@ class DonateO extends React.Component {
 }
 
 export default DonateO;
+
+const InputGroup = Input.Group;
+const { RangePicker } = DatePicker;
+
+//时间组合输入框 in 条件查询Form
+class TimeGroupInput extends React.Component {
+    static getDerivedStateFromProps(nextProps) {
+        // Should be a controlled component.
+        if ('value' in nextProps) {
+            return {
+                ...(nextProps.value || {}),
+            };
+        }
+        return null;
+    }
+    constructor(props) {
+        super(props);
+        const value = props.value || {};
+        this.state = {
+            timeType: value.timeType,
+            timeRange: value.timeRange,
+        };
+    }
+    handleSelectChange = (v) => {
+        if (!('value' in this.props)) {
+            this.setState({ timeType: v });
+        }
+        this.triggerChange({ timeType: v });
+    }
+    handleDateChange = (timeRange) => {
+        if (!('value' in this.props)) {
+            this.setState({ timeRange });
+        }
+        this.triggerChange({ timeRange });
+    }
+    triggerChange = (changedValue) => {
+        // Should provide an event to pass value to Form.
+        const onChange = this.props.onChange;
+        if (onChange) {
+            onChange(Object.assign({}, this.state, changedValue));
+        }
+    }
+    render() {
+        const timeTypeList = [{ id: "捐书时间", name: "捐书时间" }, { id: "创建时间", name: "创建时间" }];
+        return (
+            <InputGroup compact>
+                <Select
+                    value={this.state.timeType}
+                    onChange={this.handleSelectChange}
+                    style={{ width: "100px" }}
+                >
+                    {getOptionList(timeTypeList)}
+                </Select>
+                <RangePicker
+                    value={this.state.timeRange}
+                    onChange={this.handleDateChange}
+                />
+            </InputGroup>
+        );
+    }
+}
