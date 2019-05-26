5e4bc2ea4a08a939112c1ff7c9fea63a6f526166
ZhangMingZhao1 <1104272319@qq.com>
2019-05-26 18:14:56 +0800

    feat 数据权限

--- src/components/systemM/dataAuth/index.js
+++ src/components/systemM/dataAuth/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Tree, Card, Divider, Modal, Button, message} from "antd";
 import URL from "../../../api/config";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import "./index.less";
 const { TreeNode } = Tree;
 
@@ -15,15 +15,15 @@ class DataAuth extends Component {
     caseCheckedKeys: [],
     All: [],
     visible: false,
-    checkedKeys: ["100"],
-    selectedKeys: ["100"],
+    checkedKeys: [],
+    person:{},
+    uid:null,
+    flag:true,
     // flag:false,
     // seletFlag:false,
   };
 
-  componentDidMount() {
-    this.fetchInpleData();
-  }
+
 
   //获取机构信息
   fetchInpleData = uid => {
@@ -43,30 +43,39 @@ class DataAuth extends Component {
       });
   };
 
+  //获取某个人的信息
+
+  fetchPersonData =(uid)=>{
+    fetch(`${URL}/system/users/${uid}`,{credentials:"include"})
+      .then(res=>res.json())
+      .then(data=>{
+        this.setState({person:data});
+      })
+  }
   //获取要关联的全部柜子信息
   fetchAllCase = uid => {
     fetch(`${URL}/system/warehousecases/${uid}`, { credentials: "include" })
       .then(res => res.json())
       .then(data => {
         // console.log('ddddd',data.array.map((v)=>"case-"+v));
-        this.setState({caseCheckedKeys:data.array.map((v)=>"case-"+v),treeAllcase:data.datas});
+        this.setState({caseCheckedKeys:data.array.map((v)=>"case-"+v),treeAllcase:data.datas,before:data.array});
       });
   };
 
-  onCheck =(checkedKeys)=>{
-    console.log(caseCheckedKeys);
-    this.setState({ caseCheckedKeys:checkedKeys});
-    // console.log(this.state.caseCheckedKeys);
-  }
+  //勾选机构树的人
   onPersonCheck = checkedKeys =>{
     // this.setState({personCheckedKeys:checkedKeys})
     if(checkedKeys.length==1) {
       let uid = checkedKeys[0].split('-')[1];
       // console.log('uid',uid);
+      this.setState({uid:uid,flag:false});
+      this.fetchPersonData(uid);
       this.fetchPersonCaseData(uid);
       this.fetchAllCase(uid);
     }else if(checkedKeys.length>1) {
       message.warning("请只勾选一个人员");
+    }else if(checkedKeys.length<1) {
+      this.setState({flag:true});
     }
   }
 
@@ -142,13 +151,84 @@ class DataAuth extends Component {
   showModal = () => {
     this.setState({ visible: true });
   };
+
+  updatePersonCaseData = (userId,deleteCaseIds,addCaseIds)=>{
+    // console.log(JSON.stringify({
+    //   "userId":userId,
+    //   "deleteCaseIds":deleteCaseIds,
+    //   "addCaseIds":addCaseIds,
+    // }));
+    fetch(`${URL}/system/warehousecases`,{
+      credentials:"include",
+      method:"POST",
+      headers: {
+				'Content-Type': 'application/json'
+			},
+      body:JSON.stringify({
+        "userId":userId,
+        "deleteCaseIds":deleteCaseIds,
+        "addCaseIds":addCaseIds,
+      })
+    })
+      .then(res=>res.json())
+      .then(data=>{
+        // console.log('保存的返回',data);
+        if(data.code==0){
+          this.fetchPersonCaseData(userId);
+          message.success("保存成功")
+        }else {
+          message.error("保存失败，联系开发人员")
+        }
+       
+      })
+  }
+  componentDidMount() {
+    this.fetchInpleData();
+    
+  }
   handleOk = e => {
     // console.log(e);
-
+    // console.log('caseCheckedKeys',this.state.caseCheckedKeys,this.state.caseCheckedKeys.map((v)=>v.split('-')[1]))
+    let DelArray = this.diffDel(this.state.before,this.state.caseCheckedKeys.map((v)=>v.split('-')[1]-0));
+    // console.log('DelArray',DelArray);
+    let AddArray = this.diffAdd(this.state.before,this.state.caseCheckedKeys.map((v)=>v.split('-')[1]-0));
+    // console.log('AddArray',AddArray);
+    this.updatePersonCaseData(this.state.uid-0,DelArray,AddArray);
     this.setState({
       visible: false
     });
+    
   };
+
+  onCheck =(checkedKeys)=>{
+    // console.log('before',this.state.before);
+    // console.log('checkedKeys',checkedKeys);
+    // console.log('caseCheckedKeys',this.state.caseCheckedKeys);
+    this.setState({ caseCheckedKeys:checkedKeys.checked});
+    // console.log(this.state.caseCheckedKeys);
+  }
+
+  //返回删除的caseID数组
+  diffDel = (arr1,arr2)=> {
+    let tmp = [];
+    for(let i = 0; i <arr1.length; i++ ) {
+        if(arr2.indexOf(arr1[i])==-1) {
+          tmp.push(arr1[i]);
+        }
+    }
+    return tmp;
+  }
+  //返回新增的caseID数组
+  diffAdd =(arr1,arr2)=>{
+    let tmp = [];
+    for(let i = 0; i <arr2.length; i++ ) {
+      console.log(arr2[i]);
+      if(arr1.indexOf(arr2[i])==-1) {
+        tmp.push(arr2[i]);
+    }
+  }
+    return tmp;
+  }
 
   handleCancel = e => {
     // console.log(e);
@@ -162,46 +242,14 @@ class DataAuth extends Component {
   }
 
   render() {
-    const user = {
-      message: "",
-      data: {
-        uid: 2,
-        userName: "wujian",
-        telephone: "15200429735",
-        registrationtime: 1551855705000,
-        status: 1,
-        password: null,
-        beDepartment: {
-          id: 2,
-          name: "财务部"
-        },
-        beInstitution: {
-          id: 5,
-          lever: 3,
-          name: "小阳街道",
-          beInstitutions: null
-        },
-        role: "加盟商",
-        permissions: [
-          "专题管理",
-          "banner管理",
-          "信息管理",
-          "积分管理",
-          "库存查询",
-          "出库单"
-        ],
-        roleType: 1
-      },
-      session: "97A7BE6785BA1CCB802970F326C5601E",
-      fetching: false
-    };
-    // const { user } = this.props;
-    let data = user.data;
 
+    // const { user } = this.props;
+    let data = this.state.person;
+    console.log('data',data);
     // let permissions = data.permissions.toString();
-    let userName = data.userName;
-    let department = data.beInstitution.name || "无";
-    let role = data.role;
+    let userName = (!!data)?data.userName:null;
+    let beInstitution = (!!data.beInstitution)?data.beInstitution.name:null;
+    let role = (!!data)?data.role:null;
     let resDOM = this.state.treePersonCase.length!=0?this.state.treePersonCase.map((v, k) => {
       return (
         <div className="datauth-flexbox" key={k}>
@@ -222,9 +270,12 @@ class DataAuth extends Component {
     return (
       <div>
         <Modal
+          okText="保存"
+          cancelText="取消"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          // defaultExpandParent
         >
           <Tree
             checkable
@@ -237,7 +288,8 @@ class DataAuth extends Component {
         </Modal>
 
         <Card>
-          我是数据权限
+           <div className="topTips">数据权限页面提示： {this.state.flag&&"请在下方的机构树里选择一个人员"}</div>
+             
           <Divider />
           <Tree
             checkable
@@ -249,10 +301,10 @@ class DataAuth extends Component {
         <Card>
           <div className="data-content">
             <div className="onerow">
-              {userName},{department},角色：{role}
+              选择的人为：{userName},{beInstitution},该角色为：{role}
             </div>
             <div>
-              关联：<Button onClick={this.showModal}>选择</Button>
+              关联：<Button type="primary" onClick={this.showModal}>选择</Button>
             </div>
             <Divider />
             <div className="dataauth-box">
@@ -265,18 +317,18 @@ class DataAuth extends Component {
               {resDOM}
             </div>
           </div>
-          <div style={{margin:50}}><Button onClick={this.onSubmit}type="primary">保存</Button></div>
+          {/* <div style={{margin:50}}><Button onClick={this.onSubmit}type="primary">保存</Button></div> */}
         </Card>
       </div>
     );
   }
 }
 
-const mapStateToPorps = state => ({
-  user: state.getIn(["login", "user"])
-});
-
-export default connect(
-  mapStateToPorps,
-  null
-)(DataAuth);
+// const mapStateToPorps = state => ({
+//   user: state.getIn(["login", "user"])
+// });
+export default DataAuth;
+// export default connect(
+//   mapStateToPorps,
+//   null
+// )(DataAuth);
