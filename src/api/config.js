68aa7299c714cc6c677b2c2f437a1c3dd6e73966
wjq990112 <1163585385@qq.com>
2019-06-05 11:19:33 +0800

    update: 测试URL

--- src/api/config.js
+++ src/api/config.js
@@ -3,16 +3,19 @@
  *统一定义接口，有利于维护 
  * 
  **/
- 
-const base= ' https://www.easy-mock.com/mock/';
+
+const base = ' https://www.easy-mock.com/mock/';
 const SK = "5c7134c16f09752cdf0d69f4/example"
-const baseURL = base+SK;
+const baseURL = base + SK;
 
 const ceshiIP = 'http://119.3.231.11:8080';
-const ceshiURL = ceshiIP+'/yuyue';
+const ceshiURL = ceshiIP + '/yuyue';
 
 const localIP = "http://localhost:8080"
 const localURL = localIP + '/yuyue';
 
-export default ceshiURL;
+const ceshiIP2 = 'http://www.miswujian.top:8080';
+const ceshiURL2 = ceshiIP2 + '/yuyue';
+
+export default ceshiURL2;
 
